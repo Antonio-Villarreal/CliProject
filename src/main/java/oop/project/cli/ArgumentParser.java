361c96525cbaaa26b648d62b0c61a1b5338a6e3f
Antonio Villarreal <tonyjv10@gmail.com>
2024-04-13 10:31:18 -0400

    setup structure

---
+++ src/main/java/oop/project/cli/ArgumentParser.java
@@ -0,0 +1,17 @@
+package oop.project.cli;
+
+import java.util.Map;
+
+public class ArgumentParser {
+    public ArgumentParser() {}
+
+    public void addArgument() {}
+    public ArgumentParser addCommand() {}
+
+    public void parseArgs() {}
+
+    public Map<String, Object> getArgs() {}
+
+    public Object getArg() {}
+
+}
